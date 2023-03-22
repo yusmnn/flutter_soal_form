fdfb6c5c176024e4d8e5a736670249cc2dd5b4b2
yusmnn <yusmanqq@gmail.com>
2023-03-22 23:08:29 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_soal_form/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_soal_form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'contacts_page.dart';
+
+void main() {
+  runApp(const MyAppForm());
+}
